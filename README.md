e0f4d74918457f2be27f74e915d329b547f9abcb
minmax2127 <mbernales2127@gmail.com>
2025-12-14 01:01:19 +0800

    Add: word-of-the-day

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # python_simple_projects
 
 1. Log Analyzer -> given a log file, create a summary of the logs
+
+
+IDEAS
+- Project repository and randomizer
---
+++ word-of-the-day/get-word-of-the-day.py
@@ -0,0 +1,92 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+import string
+
+# constants
+MERRIAM_WEBSTER_URL = "https://www.merriam-webster.com/"
+WORD_OF_THE_DAY_URL = MERRIAM_WEBSTER_URL + "word-of-the-day/calendar"
+SEARCH_DEFINITION_URL = MERRIAM_WEBSTER_URL + "dictionary/"
+
+def get_word_definition(word):
+    # scrape definition from merriam webster website
+    response = requests.get(SEARCH_DEFINITION_URL + word)
+    content = response.content
+    soup = BeautifulSoup(content, "html.parser")
+    def_tag = soup.find("a", class_="share-icon-link d-flex justify-content-center align-items-center fb-social-link share-link fb")
+    definition = def_tag["data-share-description"]
+
+    # polish the definitions
+    index = definition.find("… See the full definition")
+    definition = definition[:index]
+    definitions = definition.split("; ")
+
+    return definitions
+    
+
+def get_random_word(no_of_words = 1):
+    # scrape word of the day url to collect words
+    response = requests.get(WORD_OF_THE_DAY_URL)
+    content = response.content
+    soup = BeautifulSoup(content, "html.parser")
+    word_containers = soup.find_all("div", class_="more-words-of-day-container")
+    
+    # save all words in a list
+    words = []
+    for container in word_containers:
+        words_tag = container.find_all("a")
+        for word in words_tag:
+            words.append(word.get_text())
+
+    # return word or list of words
+    if no_of_words == 1:
+        return random.choice(words)
+    return random.sample(words, no_of_words)
+
+def game_loop(words, choices_per_level = 5):
+    NO_OF_LIVES = 5
+    score = 0
+    lives = NO_OF_LIVES
+
+    while(lives != 0):
+        print("-" * 20)
+        print(f"LIVES: {lives}")
+        
+        # set correct answer
+        choices = random.sample(words, choices_per_level)
+        word = random.choice(choices)
+        definitions = get_word_definition(word)
+        lowercase = string.ascii_lowercase
+
+        # display definition and choices
+        print(f"\nDef: {definitions[0]}\n")
+        for i in range(len(choices)):
+            print(f"({lowercase[i]}) {choices[i]}")
+
+        # get answer
+        answer = input("\nAnswer: ")
+        if answer in lowercase[0:len(choices)]:
+            i = lowercase.index(answer)
+            # check if correct
+            if word == choices[i]:
+                score += 1
+                print("Correct!")
+            else:
+                lives -= 1
+                print(f"Incorrect! Answer: {word}")
+        else:
+            print("Invalid answer!")
+        print()
+
+    print(f"\nSCORE: {score}")
+    
+
+
+def main():
+    # quiz
+    words = get_random_word(100)
+    game_loop(words)
+
+if __name__ == "__main__":
+    main()
+    
